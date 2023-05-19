78b34b0cecd01d5f8ccda46105f581a910ef4efe
ArefinShaon <arefinshaon99@gmail.com>
2023-05-19 11:40:19 +0600

    connet to server and post done

--- index.js
+++ index.js
@@ -26,13 +26,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+
+      const toyCollection = client.db('toyDb').collection('addtoys');
+
+      app.get('/addtoys', async (req, res) => {
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+      app.post('/addtoys', async (req, res) => {
+        const addToy = req.body;
+        console.log(addToy);
+        const result = await toyCollection.insertOne(addToy);
+        res.send(result);
+    });
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
