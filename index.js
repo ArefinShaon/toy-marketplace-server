333d89fd19a087516ae694446b3b4c145e307a09
ArefinShaon <arefinshaon99@gmail.com>
2023-05-19 19:46:49 +0600

    get filtered data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,6 +34,19 @@ async function run() {
       app.get('/addtoys', async (req, res) => {
         const cursor = toyCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+      })
+    
+      app.get('/addtoys/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { rating: 1, price: 1, quantity: 1, pictureUrl: 1,name:1,sellerName:1,sellerEmail:1,description:1 },
+        };
+
+        const result = await toyCollection.findOne(query, options);
         res.send(result);
     })
 
